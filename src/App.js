88e8a41b9c907475e737e3da920daff389c0a082
Damilare Oyedeji <fuglydami@gmail.com>
2020-12-18 18:28:48 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,100 +1,72 @@
 import './index.css'
 import { useState } from 'react'
 import React from 'react'
-import { cards } from './cards'
+import { HomeTitle } from './homeTitle'
+import { data } from './data'
 
 const App = () => {
-  // const [isLoading, setIsLoading] = useState(true)
+  const Cards = ({ img, title, price, info }) => {
+    const [isInfoExpanded, setIsInfoExpanded] = useState(false)
+    const [isTourRemoved, setIsTourRemoved] = useState(true)
 
-  // if (isLoading) {
-  //   return (
-  //     <>
-  //       <h1>Loading...</h1>
-  //     </>
-  //   )
-  // }
-
-  const cardsArray = cards.map((user, i) => {
+    const toggleBtn = () => {
+      setIsInfoExpanded(!isInfoExpanded)
+    }
+    const handleClick = () => {
+      setIsTourRemoved(!isTourRemoved)
+    }
     return (
-      <Article
-        id={i}
-        img={cards[i].img}
-        title={cards[i].title}
-        price={cards[i].price}
-        synopsis1={cards[i].synopsis1}
-        synopsis2={cards[i].synopsis2}
-      />
-    )
-  })
-  return (
-    <>
-      <div className="title">
-        <h2>our tours</h2>
-        <div className="underline"></div>
-      </div>
-      {cardsArray}
-    </>
-  )
-}
+      <>
+        {isTourRemoved ? (
+          <main>
+            <section>
+              <div>
+                <article className="single-tour">
+                  <img src={img} alt={title} />
+                  <footer>
+                    <div className="tour-info">
+                      <h4 className="towns">{title}</h4>
+                      <h4 className="tour-price">{price}</h4>
+                    </div>
+                    <p>
+                      {isInfoExpanded ? info : `${info.substring(0, 200)}...`}
 
-const Article = ({ img, title, price, synopsis1, synopsis2 }) => {
-  const [deleteItem, setDeleteItem] = useState(true)
-
-  const [show, setShow] = useState(true)
-  const [hide, setHide] = useState(false)
-
-  const handleClick = () => {
-    setDeleteItem(false)
-  }
-
-  const readMore = () => {
-    setShow(!show)
-    setHide(!hide)
-  }
-  const showLess = () => {
-    setHide(!hide)
-    setShow(!show)
-  }
-
-  return (
-    <>
-      <main>
-        {deleteItem ? (
-          <section>
-            <div>
-              <article className="single-tour">
-                <img src={img} alt="photos" />
-                <footer>
-                  <div className="tour-info">
-                    <h4 className="towns">{title}</h4>
-                    <h4 className="tour-price">{price}</h4>
-                  </div>
-                  <p>
-                    {show && (
-                      <p>
-                        {synopsis1}
-                        <button onClick={readMore}> read more</button>
-                      </p>
-                    )}
-                    {hide && (
-                      <p>
-                        {synopsis2}
-                        <button onClick={showLess}> show less</button>
-                      </p>
-                    )}
-                  </p>
-
-                  <button className="delete-btn" onClick={handleClick}>
-                    not interested
-                  </button>
-                </footer>
-              </article>
-            </div>
-          </section>
+                      <button onClick={toggleBtn}>
+                        {isInfoExpanded ? ' show less' : '  read more'}
+                      </button>
+                    </p>
+                    <button className="delete-btn" onClick={handleClick}>
+                      not interested
+                    </button>
+                  </footer>
+                </article>
+              </div>
+            </section>
+          </main>
         ) : (
           []
         )}
-      </main>
+      </>
+    )
+  }
+  const dataArray = data.map((user, i) => {
+    return (
+      <>
+        <Cards
+          id={data[i].id}
+          img={data[i].img}
+          title={data[i].title}
+          price={data[i].price}
+          info={data[i].info}
+        />
+      </>
+    )
+  })
+
+  return (
+    <>
+      <HomeTitle />
+      {dataArray}
     </>
   )
 }
---
+++ src/homeTitle.js
@@ -0,0 +1,10 @@
+export const HomeTitle = () => {
+  return (
+    <>
+      <div className="title">
+        <h2>our tours</h2>
+        <div className="underline"></div>
+      </div>
+    </>
+  )
+}
